066ced3783fb540c2d7ccd81f9736401740121c5
abinercosta-cl <abinerclcosta@gmail.com>
2024-08-01 17:47:25 -0300

    api node com postgres

---
+++ server.js
@@ -0,0 +1,53 @@
+import { fastify } from "fastify";
+//import { DatabaseMemory } from "./database_memory.js";
+import { DatabasePostgres } from "./database_postgres.js";
+import "./sql.js";
+const server = fastify();
+
+//const database = new DatabaseMemory();
+const database = new DatabasePostgres();
+
+//criação de rotas padroes para um "crud"
+server.get("/videos", async (request, response) => {
+  const search = request.query.search;
+
+  const videos = await database.list(search);
+
+  return videos;
+});
+
+server.post("/videos", async (request, response) => {
+  const { title, description, duration } = request.body;
+
+  await database.create({
+    title,
+    description,
+    duration,
+  });
+
+  return response.status(201).send();
+});
+
+server.put("/videos/:id", async (request, response) => {
+  const videoId = request.params.id;
+  const { title, description, duration } = request.body;
+  await database.update(videoId, {
+    title,
+    description,
+    duration,
+  });
+  return response.status(204).send();
+});
+
+server.delete("/videos/:id", async (request, response) => {
+  const videoId = request.params.id;
+
+  await database.delete(videoId);
+
+  return response.status(204).send();
+});
+
+server.listen({
+  host: "0.0.0.0",
+  port: process.env.PORT ?? 3333,
+});
